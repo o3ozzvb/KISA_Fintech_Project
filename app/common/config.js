503b6de09a04f79ebb50c6448674c7cc86601741
passsoon <passsoon@gmail.com>
2019-02-19 13:03:46 +0900

    소스수정

--- app/common/config.js
+++ app/common/config.js
@@ -1,7 +1,11 @@
 const config = {
   serverUrl: 'https://testapi.open-platform.or.kr',
-  clientId : process.env.KISA_CLIENT_ID,
-  clientSecret: process.env.KISA_CLIENT_SECRET,
+  // clientId : process.env.KISA_CLIENT_ID,
+  // clientSecret: process.env.KISA_CLIENT_SECRET,
+  clientId: 'l7xx9b138939628c4c4488c9c50e6186325f',
+  clientSecret: 'c6cbf9058d954dc091af2839d587cf0e'
+  // clientId: 'l7xx904fd64273674bf5b5a0d3aa92e6193f',
+  // clientSecret: '182236d6d37e4df4920502af8773e802'
 };
 
 module.exports = config;
