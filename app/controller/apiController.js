7dce1b34b481d5fe7eb876b881c1bd7ef5c976cf
passsoon <passsoon@gmail.com>
2019-02-19 21:25:22 +0900

    입력 수정

--- app/controller/apiController.js
+++ app/controller/apiController.js
@@ -210,7 +210,10 @@ const insertPig = function(req,res,next){
   };
 
   pigService.insertPig(data)
-  .then(result => res.render('main2'))
+  .then(result => {
+    console.log(result);
+    res.render('main2')
+  })
   .catch( error => console.log(error));
 
   console.log("insertPig")
