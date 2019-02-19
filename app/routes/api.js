e46add567af018a9b7388d63ae2a80ef7b387a71
jskimz <kjs7920@gmail.com>
2019-02-19 14:53:26 +0900

    add api

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { realname, user_me, account_list } = require('../controller/apiController');
+const { realname, user_me, account_list,account_balance,transfer_deposit2 } = require('../controller/apiController');
 const router = express.Router();
 const isAuthenticated = require('../common/isAuthenticated');
 
@@ -9,8 +9,13 @@ router.get('/realname', isAuthenticated, realname);
 // 사용자 정보조회
 router.get('/user_me', isAuthenticated, user_me);
 
-
 // 등록계좌조회
 router.get('/account_list', isAuthenticated, account_list);
+
+//잔액조회
+router.get('/account_balance',isAuthenticated,account_balance);
+
+//입금이체
+router.get('/transfer_deposit2',isAuthenticated,transfer_deposit2);
 
 module.exports = router;
--- app/service/api/apiService.js
+++ app/service/api/apiService.js
@@ -3,78 +3,78 @@ const apiUri = require("./apiUri");
 const { getUrl } = require("../../common/util");
 
 //TODO 사용자정보조회
-const userMe = (data, config) => {
-  return axios.get(getUrl(apiUri.user_me), data, config)
+const userMe = (data) => {
+  return axios.get(getUrl(apiUri.user_me), data)
 };
 
 //TODO 사용자로그인연결 동의해제
-const unlink = (data, config) => {
-  return axios.post(getUrl(apiUri.unlink), data, config)
+const unlink = (data) => {
+  return axios.post(getUrl(apiUri.unlink), data)
 };
 
 //TODO 등록계좌조회
-const accountList = (data, config) => {
+const accountList = (data) => {
   return axios.get(getUrl(apiUri.account_list), data)
 };
 
 //TODO 계좌정보변경
-const accountUpdateInfo = (data, config) => {
-  return axios.post(getUrl(apiUri.account_update_info), data, config)
+const accountUpdateInfo = (data) => {
+  return axios.post(getUrl(apiUri.account_update_info), data)
 };
 
 //TODO 계좌해지
-const accountCancel = (data, config) => {
-  return axios.post(getUrl(apiUri.account_cancel), data, config)
+const accountCancel = (data) => {
+  return axios.post(getUrl(apiUri.account_cancel), data)
 };
 
 //TODO 잔액조회
-const accountBalance = (data, config) => {
-  return axios.get(getUrl(apiUri.account_balance), data, config)
+const accountBalance = (data) => {
+  return axios.get(getUrl(apiUri.account_balance), data)
 };
 
 //TODO 거래내역조회
-const accountTransactionList = (data, config) => {
-  return axios.get(getUrl(apiUri.account_transaction_list), data, config)
+const accountTransactionList = (data) => {
+  return axios.get(getUrl(apiUri.account_transaction_list), data)
 };
 
 //실명조회
-const realName = (data, config) => {
-  return axios.post(getUrl(apiUri.real_name), data, config)
+const realName = (data) => {
+  return axios.post(getUrl(apiUri.real_name), data)
 };
 
 //TODO 송금인정보조회
-const inqueryRemitList = (data, config) => {
-  return axios.post(getUrl(apiUri.inquery_remit_list), data, config)
+const inqueryRemitList = (data) => {
+  return axios.post(getUrl(apiUri.inquery_remit_list), data)
 };
 
 //TODO 출금이체
-const transferWithdraw = (data, config) => {
-  return axios.post(getUrl(apiUri.transfer_withdraw), data, config)
+const transferWithdraw = (data) => {
+  return axios.post(getUrl(apiUri.transfer_withdraw), data)
 };
 
 //TODO 입금이체 1
-const transferDeposit = (data, config) => {
-  return axios.post(getUrl(apiUri.transfer_deposit), data, config)
+const transferDeposit = (data) => {
+  return axios.post(getUrl(apiUri.transfer_deposit), data)
 };
 
 //TODO 입금이체 2
-const transferDeposit2 = (data, config) => {
-  return axios.post(getUrl(apiUri.transfer_deposit2), data, config)
+const transferDeposit2 = (data) => {
+  return axios.post(getUrl(apiUri.transfer_deposit2), data)
 };
 
 //TODO 이체결과조회
-const transferResult = (data, config) => {
-  return axios.post(getUrl(apiUri.transfer_result), data, config)
+const transferResult = (data) => {
+  return axios.post(getUrl(apiUri.transfer_result), data)
 };
 
 //TODO 이체 API 처리조회
-const transferRecheck = (data, config) => {
-  return axios.post(getUrl(apiUri.transfer_recheck), data, config)
+const transferRecheck = (data) => {
+  return axios.post(getUrl(apiUri.transfer_recheck), data)
 };
 
 //TODO 참가은행 상태조회
-const bankStatus = (data, config) => {
-  return axios.get(getUrl(apiUri.bank_status), data, config)
+const bankStatus = (data) => {
+  return axios.get(getUrl(apiUri.bank_status), data)
 };
 
 module.exports = {
