22b987d049136382f01a73efbb7f66574a96462e
jskimz <kjs7920@gmail.com>
2019-02-20 10:25:42 +0900

    Ddd

--- app/routes/sample.js
+++ app/routes/sample.js
@@ -44,68 +44,6 @@ router.get('/user', (req, res) => {
   res.render('index');
 });
 
-<<<<<<< HEAD
-=======
-router.get('/newlogin', (req, res) =>{
-  res.render('newlogin');
-})
-
-router.get('/main', (req,res) =>{
-  res.render('main');
-})
-
-router.get('/main2', (req,res) =>{
-  res.render('main2');
-})
-
-router.get('/main2', (req,res) =>{
-  res.render('main2');
-})
-
-router.get('/test', (req,res) =>{
-  res.render('test');
-})
-
-router.get('/threeButton', (req,res) =>{
-  res.render('threeButton');
-})
-
-router.get('/threeButton1', (req,res) =>{
-  res.render('threeButton1');
-})
-
-router.get('/reportUser', (req,res) =>{
-  res.render('reportUser');
-})
-
-router.get('/report', (req,res) =>{
-  res.render('report');
-})
-
-
-router.get('/setTarget', (req,res) =>{
-  res.render('set_target_page2');
-})
-
-router.get('/newcheckbox', (req, res) =>{
-  res.render('newcheckbox');
-})
-
-router.get('/newfinal', (req,res) =>{
-  res.render('newfinal');
-})
-
-router.get('/testmain', (req,res) =>{
-  res.render('main');
-})
-
-router.get('/newlist', (req, res) =>{
-  res.render('newlist');
-})
-
-router.get('/')
-
->>>>>>> 199a69d791e83266518152190a701bc165058714
 router.get('/insert', function (req, res, next) {
 
   const data = {
